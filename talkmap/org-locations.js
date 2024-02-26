adea8b0c3b0b5ce42c7ee8720ff3abd25ff7cdf3
Hector <bahamondeh@gmail.com>
2024-02-26 17:03:45 +0200

    edit

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,177 @@
 var addressPoints = [
+  // SPSA
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "New Orleans, USA", //SPSA 2015 (Hyatt Regency)
+    29.949545,
+    -90.076742
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "New Orleans, USA", //SPSA 2018 (Hyatt Regency)
+    29.949545,
+    -90.076742
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Vancouver, CA", // LASA 2023 (Canada)
+    49.28912777573468, -123.11687059483036
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
-  ]
+    "Copenhagen, Denmark", // NOLAN 2024 Nordic Political Science Association Conference
+    55.661830163502444, 12.59017367381444
+  ],
+  [
+    "San Francisco, USA", // LASA 2022 (Zoom!)
+    37.764191276852294, -122.44087471538701
+  ],
+  [
+    "Barcelona, Spain", // LASA 2018 (Centre de Convencions Internacional de Barcelona)
+    41.409584,
+    2.219077
+  ],
+  [
+    "New York, USA", // LASA 2016
+    40.7128,
+    -74.0059
+  ],
+   [
+  "Chicago, USA", // LASA: 2014 (Palmer House)
+    41.880646,
+    -87.627422
+  ],
+  // WPSA
+  [
+    "Las Vegas, USA", // WPSA 2015 (Caesar Palace)
+    36.116243,
+    -115.174501
+  ],
+  // APSA
+  [
+    "Washington D.C.", // APSA 2019 (Omni Shoreham Hotel)
+    38.924087,
+    -77.053621
+  ],
+  [
+    "Philadelphia, USA", // APSA 2016 (Pennsylvania Convention Center)
+    39.954973,
+    -75.159988
+  ],
+  // MPSA
+  [
+    "Chicago, USA", // MPSA: 2017 (Palmer House)
+    41.880646,
+    -87.627422
+  ],
+    // MPSA
+  [
+    "Chicago, USA", // MPSA: 2023 (Palmer House)
+    41.880646,
+    -87.627422
+  ],
+    [
+    "Chicago, USA", // MPSA: 2024 (Palmer House)
+    41.880646,
+    -87.627422
+  ],
+  // Others
+  [
+    "Santiago, Chile", // ACCP 2010 UDP
+    -33.4489,
+    -70.6693
+  ],
+    [
+    "Santiago, Chile", // ACCP 2023 Usach
+  -33.451022, 
+  -70.685904
+  ],
+
+  [
+    "Concepcion, Chile", // INPAE
+    -36.494150,
+    -73.030493
+  ],
+  // REPAL)
+  [
+    "New Orleans, LA", // 2019
+    29.939427,
+    -90.121318
+  ],
+    // SPSA
+  [
+    "New Orleans, LA", //  2024
+    29.946611666798276, 
+    -90.0637500577194
+  ],
+  // AISPE)
+  [
+    "Bologna, It", // 2019
+    44.497150,
+    11.354028
+  ],
+// MPSA
+  [
+    "Chicago, USA", // MPSA: 2020 (Palmer House)
+    41.880646,
+    -87.627422
+  ],
+  // EPSA
+  [
+    "Prague, Czech Republic", // EPSA: 2022 (Prague Congress Centre)
+    50.062439155334545,
+    14.429037832429705
+  ],
+  [
+    "Glasgow, Scotland", // EPSA: 2023 (University of Strathclyde,The Technology and Innovation Centre)
+    55.86053320408491, 
+    -4.242903124728601
+  ],
+  [
+    "Glasgow, Scotland", // EPSA: 2024 (University of Strathclyde,The Technology and Innovation Centre)
+    50.94876148678886, 
+    6.94348770962405
+  ],
+// ECPR
+  [
+    "Inssbruck, Austria", // ECPR: 2022 (University of Innsbruck)
+    47.26184826224502, 11.385602383668347
+  ],
+  // NSA
+  [
+    "Reykjavik, Iceland", // NSA: 2022 (University of Iceland)
+    64.13926337754505, -21.951501132825474
+  ],
+  // Abo Akademi
+  [
+    "Turku, Finland", // 2022 (Abo Akademi, Zoom)
+    60.45091001824319, 22.27774630764093
+  ],
+  // University of Turku, Political Science
+  [
+    "Turku, Finland", // 2021 (University of Turku, Zoom)
+    60.4584205391681, 22.28650237521656
+  ],
+  // University of Turku, Economics
+  [
+    "Turku, Finland", // 2021 (University of Turku, TSE, Zoom)
+    60.45477362543441, 22.289130142625428
+  ],
+  // University of Turku, INVEST Workshop on Experiments
+  [
+    "Turku, Finland", // 2021 (INVEST Workshop on Experiments, Zoom)
+    60.45477362543441, 22.289130142625428
+  ],
+  // University of Turku, INVEST Research Seminar
+  [
+    "Turku, Finland", // 2021 (INVEST Research Seminar, Zoom)
+    60.4489235638133, 22.293786371517424
+  ],
+    //  Tradewinds Island Resort, st petersburg miami, SPSA 2023
+  [
+    "St. Petersburg, FL, US",
+    27.729741, -82.743840
+  ],
+
+  // NOPSA BERGEN Norway, NOPSA 2024
+  [
+    "Bergen, Norway",
+      60.39798604545595, 5.322784822513619
+  ],
 ];
